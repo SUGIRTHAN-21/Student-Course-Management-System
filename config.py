02f9bcefb958d1746c37110244bfd90a91597517
SUGIRTHAN-21 <j.sugirthan2006@gmail.com>
2025-06-24 09:00:33 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Database configuration
-DB_CONFIG = {
-    'host': 'localhost',
-    'database': 'student_course_management',
-    'user': 'root',
-    'password': 'Data_Scientist@45.',  # Change this to your MySQL password
-    'port': 3306
-}
